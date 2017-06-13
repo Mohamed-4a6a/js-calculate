f3f5de02e93c98acf3f5586317c1988fab75b6f7
Mohamed-4a6a <510073@edu.rocmn.nl>
2017-06-14 00:50:20 +0200

    nu werkt het pas

---
+++ script.js
@@ -0,0 +1,32 @@
+function uitRekenen() {
+  var nummer = document.getElementById('numbers').value;
+  var split = nummer.split(",");
+  var operator = document.getElementById('operation').value;
+  var result = "";
+
+  for (i = 0; i < split.length; i++) {
+    split[i] = parseFloat(split[i])
+  };
+
+  var result = split[0];
+  for (var i=1; i < split.length; i++) {
+      result = reken(result, split[i], operator);
+  };
+
+
+
+  function reken(nbrOne, nbrTwo, operator) {
+      if (operator == '+') {
+          result = nbrOne + nbrTwo;
+      } else if (operator == '-') {
+          result = nbrOne - nbrTwo;
+      } else if (operator == '*') {
+          result = nbrOne * nbrTwo;
+      } else if (operator == '/') {
+          result = nbrOne / nbrTwo;
+      }
+        return result;
+     }
+
+      document.getElementById('result').innerHTML = result;
+};
